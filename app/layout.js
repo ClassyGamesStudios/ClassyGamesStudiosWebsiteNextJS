23749311d00acb3dcc616d439d6829e3940b0f91
Jamie <jamie@classygames.gg>
2025-10-24 22:33:59 +0300

    Updated favicon and integrated Glitch analytics

--- app/layout.js
+++ app/layout.js
@@ -3,20 +3,30 @@ import Navbar from "@/components/navbar";
 import "@/styles/globals.css";
 
 export const metadata = {
-  title: "Classy Games Studio",
-  description: "Official website of Classy Games Studio.",
+  title: "Classy Games",
+  description: "Rediscover The Joy of Play.",
   icons: {
     icon: "./Favicons/main-logo.png",
   },
 };
+
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
+      <html lang="en">
+      <head>
+        {/* Glitch Analytics Script */}
+        <script
+            src="https://api.glitch.fun/js/game-analytics.js"
+            data-title-id="af3cddfe-1a50-48f3-a268-6339fac33fba"
+            data-tracking-token="null"
+            async
+        />
+      </head>
       <body className={`antialiased`}>
-        <Navbar />
-        {children}
-        <Footer />
+      <Navbar />
+      {children}
+      <Footer />
       </body>
-    </html>
+      </html>
   );
 }
