9990b578915e3df673daec68aafcc0b4bc47734d
Jamie <jamie@classygames.gg>
2025-10-28 22:21:41 +0300

    Updated tracking token

--- app/layout.js
+++ app/layout.js
@@ -18,7 +18,7 @@ export default function RootLayout({ children }) {
         <script
             src="https://api.glitch.fun/js/game-analytics.js"
             data-title-id="af3cddfe-1a50-48f3-a268-6339fac33fba"
-            data-tracking-token="null"
+            data-tracking-token="628d0ab2fb3c4f23f807d42bbafb81bc46261d5e44f47899b240756e9ae39254"
             async
         />
       </head>
