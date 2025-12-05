e82a6a47759f8d69272fa124cf0a8ccb70e1f9e4
Jamie <jamie@classygames.gg>
2025-12-05 14:52:13 +0530

    Removed Glitch tracking

--- app/layout.js
+++ app/layout.js
@@ -18,36 +18,29 @@ export const metadata = {
 };
 
 export default function RootLayout({ children }) {
-  return (
-      <html lang="en">
-      <head>
-        {/* Glitch Analytics Script */}
-        <script
-            src="https://api.glitch.fun/js/game-analytics.js"
-            data-title-id="af3cddfe-1a50-48f3-a268-6339fac33fba"
-            data-tracking-token="628d0ab2fb3c4f23f807d42bbafb81bc46261d5e44f47899b240756e9ae39254"
-            async
+    return (
+        <html lang="en">
+        <head></head>
+        <body className="antialiased">
+        {/* Google Tag Manager */}
+        <Script
+            src="https://www.googletagmanager.com/gtag/js?id=G-FWJWQD4N11"
+            strategy="afterInteractive"
         />
-      </head>
-      <body className={`antialiased`}>
-      {/* Google Tag Manager */}
-      <Script
-        src="https://www.googletagmanager.com/gtag/js?id=G-FWJWQD4N11"
-        strategy="afterInteractive"
-      />
-      <Script id="google-analytics" strategy="afterInteractive">
-        {`
-          window.dataLayer = window.dataLayer || [];
-          function gtag(){dataLayer.push(arguments);}
-          gtag('js', new Date());
-          gtag('config', 'G-FWJWQD4N11');
-        `}
-      </Script>
-      <ClarityAnalytics />
-      <Navbar />
-      {children}
-      <Footer />
-      </body>
-      </html>
-  );
+        <Script id="google-analytics" strategy="afterInteractive">
+            {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-FWJWQD4N11');
+          `}
+        </Script>
+
+        <ClarityAnalytics />
+        <Navbar />
+        {children}
+        <Footer />
+        </body>
+        </html>
+    );
 }
